0c7d1831bf4d8310f37356011c1cfb5e80eb93c0
Abdeljalil NACEUR <contact@naceur-abdeljalil.com>
2020-04-06 13:45:58 +0200

    TEST HELLO CHANGED

--- client/src/components/Footer.compoenent.jsx
+++ client/src/components/Footer.compoenent.jsx
@@ -10,7 +10,7 @@ class Footer extends React.Component {
                         <div class="col-lg-6">
                             <div class="copyright text-center mt-35">
                                 <p class="text">
-                                    MaClasse - All rights reserved 2020
+                                    Clacile - All rights reserved 2020
                             </p>
                             </div>
                         </div>
