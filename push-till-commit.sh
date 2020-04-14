e6c5672996778fde08625120f453352050d97ce9
Abdeljalil NACEUR <contact@naceur-abdeljalil.com>
2020-04-14 17:18:07 +0200

    TEST 1

--- push-till-commit.sh
+++ push-till-commit.sh
@@ -2,4 +2,4 @@
 SHA_COMMIT_HASH=$1
 BRANCH_NAME=$2
 echo "Calling git push origin $SHA_COMMIT_HASH:$BRANCH_NAME"
-git push origin $SHA_COMMIT_HASH:$BRANCH_NAME
+git push origin 9eb11f70fffd468fb5ee71c4fda6c6254682f28c:feature/CYC-1355
